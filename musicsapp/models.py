f78608e1baab8ddcb99fd0c821d1a55ec417c945
Adeniji Ridwan Opeyemi <111622920+adenijiridwan43@users.noreply.github.com>
2022-10-27 20:42:56 -0700

    second commit

--- musicsapp/models.py
+++ musicsapp/models.py
@@ -1,3 +1,35 @@
+from datetime import datetime
+from email.policy import default
 from django.db import models
 
 # Create your models here.
+
+class Artiste (models.Model) :
+    # A class name for Artist profile sit here
+
+    first_name = models.CharField( max_length=200)
+    second_name = models.CharField( max_length=200)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
+
+
+class Song (models.Model) :
+    # A class name for Song profile sit here
+    title = models.CharField( max_length=200)
+    date_release = models.DateField( default=datetime.today)
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __str__(self):
+            return self.title
+
+
+class Lyrics (models.Model) : 
+    # A class name for Lyrics profile sit here
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
--- thesongcrud/settings.py
+++ thesongcrud/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # this is for my musicsapp
+    'musicsapp' ,
 ]
 
 MIDDLEWARE = [
